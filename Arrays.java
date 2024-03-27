363b31616b24481e3a8cba283771a9fe92096193
Sonu Saroj <sonusaroj02010@gmail.com>
2024-03-27 22:42:40 +0530

    arrays

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays{
+
+    public static void main(String a[]){
+
+        int nums[] = {2,3,3,4,5};
+        nums[1]=33; // updating array element
+
+        System.out.println(nums[1]);
+
+        // another syntax:
+
+        // int nums[] = new int[4];
+        // nums[0] = 1;
+        // nums[1]=2
+        // nums[2] = 1;
+        // nums[3]=2
+        
+    }
+
+
+}
