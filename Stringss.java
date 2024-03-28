2ca9f4244ef7423cfa2ec2637d9c3819d797e058
Sonu Saroj <sonusaroj02010@gmail.com>
2024-03-28 22:22:37 +0530

    Stings

---
+++ Stringss.java
@@ -0,0 +1,23 @@
+public class Stringss{
+    public static void main(String a[]){
+        String name ="sonu";
+        //System.out.println("hello "+name);
+
+        // String name = new String("sonu");
+        // System.out.println(name);
+
+        // indexing
+        //System.out.println(name.charAt(0));
+
+        //single object is created 
+        String s1 ="sonu";
+        String s2 = "sonu";
+        //System.out.println(s1==s2);
+
+        //STRING BUFFER
+        StringBuffer sb = new StringBuffer("sonu");
+        sb.append(" Saroj");
+        System.out.println(sb);
+
+    }
+}
